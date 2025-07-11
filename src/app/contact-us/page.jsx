de153a14dcc1741ef234894d3b2c7c70b429b46a
Ooozari <uzairasiff1227@gmail.com>
2025-07-11 12:20:47 +0500

    added break points for contact form

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -8,16 +8,26 @@ function Contact() {
     return (
         <>
             {/* HeroSection */}
-            <div className="relative w-full h-[300px] md:h-[400px] lg:h-[500px] overflow-hidden">
+            <div className="relative w-full h-3/4 overflow-hidden">
                 {/* Background image */}
                 <Image
                     src={ContactPageHeroSection}
                     alt="Contact page banner"
+                    fill
+                    className="object-cover"
                     priority
                 />
 
-                {/* Gradient overlay */}
-                <div className="absolute top-1/2 inset-0 bg-gradient-to-b from-[#1E365C] to-[#1E365Cf0]" />
+                {/* Gradient + blur overlay */}
+                <div
+                    className="absolute inset-0"
+                    style={{
+                        background:
+                            "linear-gradient(180deg, #1E365C 0%, #1E365C 87.89%, rgba(30, 54, 92, 0.94) 100%)",
+                        backdropFilter: "blur(250px)",
+                        WebkitBackdropFilter: "blur(250px)",
+                    }}
+                ></div>
 
                 {/* Centered heading */}
                 <div className="absolute inset-0 flex items-center justify-center">
@@ -27,14 +37,14 @@ function Contact() {
 
             {/* Headings */}
             <div className='text-center mb-9'>
-                <h1 className='text-Black text-3xl font-extrabold mb-2'>HELLO! HAVE <span className='text-Secondary'>
+                <h1 className='text-Black text-xl sm:text-2xl md:text-3xl font-extrabold mb-2'>HELLO! HAVE <span className='text-Secondary'>
                     A QUESTION? </span>
                 </h1>
-                <p className='px-3'>We’re here to help. If you have any questions or simply want to chat, let us know below. We’ll always reply quickly!</p>
+                <p className='px-3 text-darkGrey' >We’re here to help. If you have any questions or simply want to chat, let us know below. We’ll always reply quickly!</p>
             </div>
             {/* Form */}
             <div className='flex flex-col mx-auto gap-4 w-[70%]'>
-                <div className='flex gap-3'>
+                <div className='flex flex-col gap-3 sm:flex-row'>
                     <div className='w-full flex flex-col gap-2 '>
                         <label >First Name</label>
                         <Input type='text' placeholder="Write" />
