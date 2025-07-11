28732826f989c86aeed3c40f01eb6839b4d5c130
Ooozari <uzairasiff1227@gmail.com>
2025-07-11 13:29:22 +0500

    develped Contact Page

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -8,24 +8,20 @@ function Contact() {
     return (
         <>
             {/* HeroSection */}
-            <div className="relative w-full h-3/4 overflow-hidden">
+            <div className="relative w-full overflow-hidden">
                 {/* Background image */}
                 <Image
                     src={ContactPageHeroSection}
                     alt="Contact page banner"
-                    fill
                     className="object-cover"
                     priority
                 />
 
-                {/* Gradient + blur overlay */}
                 <div
                     className="absolute inset-0"
                     style={{
                         background:
-                            "linear-gradient(180deg, #1E365C 0%, #1E365C 87.89%, rgba(30, 54, 92, 0.94) 100%)",
-                        backdropFilter: "blur(250px)",
-                        WebkitBackdropFilter: "blur(250px)",
+                            "linear-gradient(180deg, rgba(30, 54, 92, 0) 0%, #1E365C 100%)",
                     }}
                 ></div>
 
@@ -35,30 +31,38 @@ function Contact() {
                 </div>
             </div>
 
+            {/* Gradient continuation after hero image */}
+            <div
+                className="w-full h-[100px]" // Adjust height as needed
+                style={{
+                    background:
+                        "linear-gradient(0deg, rgba(30, 54, 92, 0) 0%, #1E365C 100%)",
+                }}
+            ></div>
             {/* Headings */}
             <div className='text-center mb-9'>
-                <h1 className='text-Black text-xl sm:text-2xl md:text-3xl font-extrabold mb-2'>HELLO! HAVE <span className='text-Secondary'>
+                <h1 className='text-Black text-2xl sm:text-3xl md:text-4xl font-extrabold mb-2'>HELLO! HAVE <span className='text-Secondary'>
                     A QUESTION? </span>
                 </h1>
-                <p className='px-3 text-darkGrey' >We’re here to help. If you have any questions or simply want to chat, let us know below. We’ll always reply quickly!</p>
+                <p className='px-2 text-darkGrey text-sm md:text-md'>We’re here to help. If you have any questions or simply want to chat, let us know below. We’ll always reply quickly!</p>
             </div>
             {/* Form */}
             <div className='flex flex-col mx-auto gap-4 w-[70%]'>
                 <div className='flex flex-col gap-3 sm:flex-row'>
-                    <div className='w-full flex flex-col gap-2 '>
+                    <div className='w-full flex flex-col gap-1 md:gap-2'>
                         <label >First Name</label>
                         <Input type='text' placeholder="Write" />
                     </div>
-                    <div className='w-full flex flex-col gap-2 '>
+                    <div className='w-full flex flex-col gap-1 md:gap-2 '>
                         <label >Last Name</label>
                         <Input type='text' placeholder="Write" />
                     </div>
                 </div>
-                <div className='flex flex-col gap-2'>
+                <div className='flex flex-col gap-1 md:gap-2'>
                     <label >Email Address</label>
                     <Input type='email' placeholder="Write" />
                 </div>
-                <div className='flex flex-col gap-2'>
+                <div className='flex flex-col gap-1 md:gap-2'>
                     <label >Message</label>
                     <Input type='text' placeholder="Write..." />
                 </div>
